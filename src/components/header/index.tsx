e24035235e8be96fccd592f4c73fc79ae8f92a97
steph-crown <emmanuelstephen024@gmail.com>
2021-12-30 13:20:42 +0100

    did nav component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,21 +4,40 @@ import { IHeaderProps } from "./interface";
 import { HeaderContainer } from "./style";
 
 export const Header: FC<IHeaderProps> = ({ curr }) => {
+    const navLinks: { name: string; link: string }[] = [
+        { name: "About", link: "/" },
+        { name: "Projects", link: "/" },
+        { name: "Resume", link: "/" },
+        { name: "Contact Me", link: "/" },
+    ];
     return (
         <HeaderContainer>
-            <Link className="logo" to="/">
-                <h6 className="w">S</h6>
-                <h6 className="w">T</h6>
-                <h6 className="w">E</h6>
-                <h6 className="w">P</h6>
-                <h6 className="w">H</h6>
+            <div className="flex">
+                <Link className="logo" to="/">
+                    <h6 className="w">S</h6>
+                    <h6 className="w">T</h6>
+                    <h6 className="w">E</h6>
+                    <h6 className="w">P</h6>
+                    <h6 className="w">H</h6>
 
-                <h6 className="space-left y">C</h6>
-                <h6 className="y">R</h6>
-                <h6 className="y">O</h6>
-                <h6 className="y">W</h6>
-                <h6 className="y">N</h6>
-            </Link>
+                    <h6 className="space-left y">C</h6>
+                    <h6 className="y">R</h6>
+                    <h6 className="y">O</h6>
+                    <h6 className="y">W</h6>
+                    <h6 className="y">N</h6>
+                </Link>
+                <nav className="flex">
+                    {navLinks.map((link, index) => (
+                        <Link
+                            key={link.name}
+                            to={link.link}
+                            className={curr.includes(index) ? "current" : ""}
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
+                </nav>
+            </div>
         </HeaderContainer>
     );
 };
--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -7,10 +7,20 @@ export const HeaderContainer = styled.div`
     @media (max-width: 480px) {
         padding: 40px var(--side-pad);
     }
+
+    &,
+    & .flex {
+        display: flex;
+        min-height: 2px;
+        align-items: center;
+    }
+    justify-content: space-between;
+    width: 100%;
     a.logo {
         display: flex;
         min-height: 2px;
         align-items: center;
+        margin-right: 72px;
 
         h6 {
             color: var(--white);
@@ -24,6 +34,25 @@ export const HeaderContainer = styled.div`
             &.space-left {
                 margin-left: 6px;
             }
+        }
+    }
+
+    nav.flex {
+        a {
+            color: var(--shade-1);
+            font-weight: 500;
+            font-size: 1em;
+            display: block;
+            margin-right: 40px;
+
+            &.current {
+                color: var(--white);
+                font-weight: 600;
+            }
+        }
+
+        @media (max-width: 700px) {
+            display: none;
         }
     }
 `;
